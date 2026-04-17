7a9ac611eb94fd6263515f3b0f15effe715c21e8
Somwshwar <sonusolat512@gmail.com>
2026-04-17 13:51:42 +0530

    chnages hosting code

--- server.js
+++ server.js
@@ -170,6 +170,8 @@ app.post("/chat", (req, res) => {
 });
 
 // Start Server
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on port " + PORT);
 });
